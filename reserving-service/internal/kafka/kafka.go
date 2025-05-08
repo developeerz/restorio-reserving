d8b5672623dc22976f8a598b105cc1eecf9d4e43
mikhail <mihaelvozhzhov@gmail.com>
2025-05-08 20:52:42 +0700

    TDM-135 Добавил кафку

---
+++ reserving-service/internal/kafka/kafka.go
@@ -0,0 +1,41 @@
+package kafka
+
+import (
+	"context"
+	"time"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type Kafka struct {
+	writer *kafka.Writer
+}
+
+func NewKafka(brokers []string) *Kafka {
+	return &Kafka{
+		writer: &kafka.Writer{
+			Addr:         kafka.TCP(brokers...),
+			Balancer:     &kafka.LeastBytes{},
+			MaxAttempts:  5,
+			WriteTimeout: 10 * time.Second,
+			Async:        false,
+			RequiredAcks: kafka.RequireAll,
+			BatchSize:    1,
+		},
+	}
+}
+
+func (k *Kafka) Send(ctx context.Context, topic string, key string, payload []byte) error {
+	return k.writer.WriteMessages(
+		ctx,
+		kafka.Message{
+			Topic: topic,
+			Key:   []byte(key),
+			Value: payload,
+		},
+	)
+}
+
+func (k *Kafka) Close() error {
+	return k.writer.Close()
+}
