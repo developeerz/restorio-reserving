1961f93360a2913e69a582b26beed75c7cf77347
mikhail <mihaelvozhzhov@gmail.com>
2025-05-09 12:19:21 +0700

    TDM-138 Добавил транзакцию на отправку

--- reserving-service/internal/repository/postgres/outbox.go
+++ reserving-service/internal/repository/postgres/outbox.go
@@ -2,22 +2,33 @@ package postgres
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
 	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
 )
 
-type OutboxRepository struct {
-	db *sqlx.DB
+type OutboxRepository interface {
+	UpdateSendStatusTrueByID(ctx context.Context, id uuid.UUID) error
+	Transaction(ctx context.Context, fn func(repo OutboxRepository) error) error
 }
 
-func NewOutboxRepository(db *sqlx.DB) *OutboxRepository {
-	return &OutboxRepository{
+type outboxRepository struct {
+	db sqlx.ExtContext
+}
+
+func NewOutboxRepository(db *sqlx.DB) OutboxRepository {
+	return &outboxRepository{
 		db: db,
 	}
 }
 
-func (r *OutboxRepository) UpdateSendStatusTrueByID(ctx context.Context, id uuid.UUID) error {
+type txoutboxRepository struct {
+	tx *sqlx.Tx
+}
+
+func (r *outboxRepository) UpdateSendStatusTrueByID(ctx context.Context, id uuid.UUID) error {
 	query := `
 		UPDATE outbox
 		SET send_status = true
@@ -26,4 +37,46 @@ func (r *OutboxRepository) UpdateSendStatusTrueByID(ctx context.Context, id uuid
 	_, err := r.db.ExecContext(ctx, query, id)
 
 	return err
+}
+
+func (r *outboxRepository) Transaction(ctx context.Context, fn func(repo OutboxRepository) error) error {
+	db, ok := r.db.(*sqlx.DB)
+	if !ok {
+		return errors.New("cannot start transaction")
+	}
+
+	tx, err := db.BeginTxx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	var rbErr error
+
+	defer func() {
+		if rbErr != nil {
+			if err := tx.Rollback(); err != nil {
+				rbErr = fmt.Errorf("original error: %v, rollback error: %w", rbErr, err)
+			}
+		}
+	}()
+
+	repo := r.withTx(tx)
+
+	err = fn(repo)
+	if err != nil {
+		rbErr = err
+		return err
+	}
+
+	if err := tx.Commit(); err != nil {
+		return fmt.Errorf("commit failed: %w", err)
+	}
+
+	return nil
+}
+
+func (r *outboxRepository) withTx(tx *sqlx.Tx) *outboxRepository {
+	return &outboxRepository{
+		db: tx,
+	}
 }
--- reserving-service/internal/scheduler/scheduler.go
+++ reserving-service/internal/scheduler/scheduler.go
@@ -3,17 +3,18 @@ package scheduler
 import (
 	"context"
 
+	"github.com/developeerz/restorio-reserving/reserving-service/internal/repository/postgres"
 	"github.com/developeerz/restorio-reserving/reserving-service/internal/repository/postgres/entity/outbox"
 	"github.com/go-co-op/gocron/v2"
 )
 
 type Scheduler struct {
 	sender     Sender
-	outboxRepo OutboxRepository
+	outboxRepo postgres.OutboxRepository
 	scheduler  gocron.Scheduler
 }
 
-func New(sender Sender, outboxRepo OutboxRepository) (*Scheduler, error) {
+func New(sender Sender, outboxRepo postgres.OutboxRepository) (*Scheduler, error) {
 	scheduler, err := gocron.NewScheduler()
 	if err != nil {
 		return nil, err
@@ -43,11 +44,18 @@ func (s *Scheduler) ScheduleJob(ctx context.Context, outboxMessage outbox.Entity
 	return nil
 }
 
-func sendMessageJob(ctx context.Context, sender Sender, repo OutboxRepository, outboxMessage outbox.Entity) error {
-	err := sender.Send(ctx, outboxMessage.Topic, outboxMessage.Payload)
-	if err != nil {
-		return err
-	}
+func sendMessageJob(ctx context.Context, sender Sender, repo postgres.OutboxRepository, outboxMessage outbox.Entity) error {
+	return repo.Transaction(ctx, func(repo postgres.OutboxRepository) error {
+		err := repo.UpdateSendStatusTrueByID(ctx, outboxMessage.ID)
+		if err != nil {
+			return err
+		}
 
-	return repo.UpdateSendStatusTrueByID(ctx, outboxMessage.ID)
+		err = sender.Send(ctx, outboxMessage.Topic, outboxMessage.Payload)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
 }
