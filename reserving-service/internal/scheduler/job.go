1c1a76d1144be169c01f942aacef4fd64be12c85
mikhail <mihaelvozhzhov@gmail.com>
2025-05-12 18:48:45 +0700

    TDM-138 Удалил print в stdout

--- reserving-service/internal/scheduler/job.go
+++ reserving-service/internal/scheduler/job.go
@@ -2,7 +2,7 @@ package scheduler
 
 import (
 	"context"
-	"log"
+	"fmt"
 
 	"github.com/developeerz/restorio-reserving/reserving-service/internal/repository/postgres"
 	"github.com/developeerz/restorio-reserving/reserving-service/internal/repository/postgres/entity/outbox"
@@ -10,17 +10,14 @@ import (
 
 func sendMessageJob(ctx context.Context, sender Sender, repo postgres.OutboxRepository, outboxMessage outbox.Entity) error {
 	return repo.Transaction(ctx, func(repo postgres.OutboxRepository) error {
-		log.Print("start send job")
 		err := repo.UpdateSendStatusTrueByID(ctx, outboxMessage.ID)
 		if err != nil {
-			log.Printf("job error: %v", err)
-			return err
+			return fmt.Errorf("job error: %v", err)
 		}
 
 		err = sender.Send(ctx, outboxMessage.Payload)
 		if err != nil {
-			log.Printf("job error: %v", err)
-			return err
+			return fmt.Errorf("job error: %v", err)
 		}
 
 		return nil
